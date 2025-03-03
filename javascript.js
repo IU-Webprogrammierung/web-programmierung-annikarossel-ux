f7e3afb0f062db31591b291ea533269d4b744f8d
Annika Rossel <annika.rossel@iu-study.org>
2025-03-03 18:48:22 +0100

    Hinzufügen Suchfunktion für Reiseziele - Refactoring JavaScript file

--- javascript.js
+++ javascript.js
@@ -1,8 +1,35 @@
-function myFunction() {
-    var x = document.getElementById("NavigationBar");
-    if (x.className === "NavBar") {
-      x.className += " responsive";
+
+// Fügt dem Klassenname das Attribut responsive hinzu
+function createResponsiveNavigation() {
+    var NavigationBar = document.getElementById("NavigationBar");
+    if (NavigationBar.className === "NavBar") {
+      NavigationBar.className += " responsive";
     } else {
-      x.className = "NavBar";
+      NavigationBar.className = "NavBar";
     }
   }
+
+
+
+// Aufsetzen Funktion, die auf losgelassene Taste reagiert
+const searchInputElement = document.getElementById("search");
+searchInputElement.addEventListener("keyup", function() {
+InputValue = searchInputElement.value;
+filterList();
+});
+
+// Versteckt alle Karten, die den gesuchten Begriff nicht beinhalten
+function filterList(){
+    const filter = InputValue.toLowerCase()
+    const listItems = document.querySelectorAll(".ListItem");
+
+    listItems.forEach((item) =>{
+        let text = item.textContent.toLowerCase();
+        if(text.includes(filter)){
+            item.parentNode.style.display ='';
+        }
+        else{
+            item.parentNode.style.display ='none';
+        }
+    })
+}
